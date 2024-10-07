60dfcd77a9d175c4ddc713b80e7475f4696b2fb2
andero-magi <158137801+andero-magi@users.noreply.github.com>
2024-10-07 14:09:19 +0300

    Add models and database context

---
+++ FinantsApp/Models/Transaction.cs
@@ -0,0 +1,29 @@
+﻿namespace FinantsApp.Models;
+
+public class Transaction
+{
+    public int Id { get; set; }
+
+    public decimal Amount { get; set; }
+    public TransactionReason Reason { get; set; }
+    public string? Description { get; set; }
+    public DateTime Date { get; set; }
+
+    public bool IsIncome => Amount > 0;
+}
+
+public enum TransactionReason
+{
+    // Income Reasons
+    Salary,
+    Bonus,
+    Gift,
+    InvestmentReturn,
+
+    // Expense Reasons
+    LivingCosts,
+    Entertainment,
+    Transportation,
+
+    Other
+}
